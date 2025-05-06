28240b81d8f195971721e6a19cb2aa1487ba14a0
Dara-star-boop <bojidarovadara@gmail.com>
2025-05-06 18:01:50 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n,sum=0,i=0,x;
-	int *nums;
+	int n, sum = 0, i = 0;
+	int* nums;
 	cout << "vuvedete broi elementi: ";
 	cin >> n;
-	
+
 	if (n > 0 && n < 100) {
 		nums = new int[n];
 	}
@@ -14,17 +14,14 @@ int main() {
 		return 0;
 	}
 	while (i < n) {
-		cout << "Vuvedete element"<<i<<":";
+		cout << "Vuvedete element" << i << ":";
 		cin >> nums[i];
 		i++;
 	}
-	cout << "Vuvedete chisloto X ";
-	cin >> x;
-	
 	for (i = 0; i < n; i++) {
-		if (nums[i]==x) {
-			sum++;
+		if (nums[i] > 0) {
+			sum = nums[i] + sum;
 		}
 	}
-	cout << "Chisloto X se sreshta " << sum<<" puti";
+	cout << "Sumata e " << sum;
 }
